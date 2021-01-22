6f0e4835bfc7141b9016aa3b6325697c54da461d
NunoCos <nuno18_9@hotmail.com>
2021-01-22 19:53:46 +0000

    add random sequnce and set variables

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,7 @@
 userSeq = [];
 simonSeq = [];
 var id,color, level = 0;
+const NUM_OF_LEVELS = 20;
 var boardsound = [
     "https://www.soundjay.com/button/sounds/button-37.mp3",
     "https://www.soundjay.com/button/sounds/beep-06.mp3",
@@ -11,6 +12,7 @@ var boardsound = [
 
 //start sequence
 $(document).ready(function() {
+    $("#score").text("");
     $("#start").click(function() {
         level++;
         startSequence();
@@ -22,14 +24,28 @@ function startSequence() {
     getRandomNum();
     var i = 0;
     var myInterval = setInterval(function() {
-        
+      id = simonSeq[i];
+      color = $("#"+id).attr("class").split(" ")[1];
+      console.log(id+" "+color);
+      addClassSound(id, color);
+      i++;
+      if(i == simonSeq.length) {
+        clearInterval(myInterval);
+      }
     },1000);
-}
-
-
+    }
 
 //generate random sequence
 function getRandomNum() {
     var random = Math.floor(Math.random() * 4);
     simonSeq.push(random);
 }
+
+function addClassSound(id, color) {
+    $("#"+id).addClass(color+".active");
+    playSound()
+    setTimeout(function(){
+        $("#"+id).removeClass(color+".active");
+    }, 500);
+};
+
