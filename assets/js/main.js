1b8f11b906ff847452f47a477c3d8b654d71c748
NunoCos <nuno18_9@hotmail.com>
2021-01-22 18:19:16 +0000

    create basic game js code

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,35 @@
-var colourArray = ["#1", "#2", "#3", "#4"];
-var gameSequence = [];
-var userSequence = [];
-var gameScore = 0;
+//variables
+userSeq = [];
+simonSeq = [];
+var id,color, level = 0;
+var boardsound = [
+    "https://www.soundjay.com/button/sounds/button-37.mp3",
+    "https://www.soundjay.com/button/sounds/beep-06.mp3",
+    "https://www.soundjay.com/misc/sounds/censor-beep-01.mp3",
+    "https://www.soundjay.com/button/sounds/button-3.mp3"
+]
 
+//start sequence
+$(document).ready(function() {
+    $("#start").click(function() {
+        level++;
+        startSequence();
+    })
+})
+
+function startSequence() {
+    $("#score").text(level);
+    getRandomNum();
+    var i = 0;
+    var myInterval = setInterval(function() {
+        
+    },1000);
+}
+
+
+
+//generate random sequence
+function getRandomNum() {
+    var random = Math.floor(Math.random() * 4);
+    simonSeq.push(random);
+}
