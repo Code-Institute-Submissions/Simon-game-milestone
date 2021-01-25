6bba28b27bde7d1ba5d5030a53912780121bf557
NunoCos <nuno18_9@hotmail.com>
2021-01-25 18:40:42 +0000

    remove email js part

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,36 +1,18 @@
 //Variables
 var colourArray = ["#blue", "#green", "#red", "#yellow"];
-var gameSequence = [];
-var userSequence = [];
+var gameSeq = [];
+var userSeq = [];
 //user starts game with score = 0
 var gameScore = 0;
 
 //Start game sequence
 $("#start").click(function() { 
     var rand = colourArray[Math.floor(Math.random() * 4)];
-    gameSequence.push(rand);
+    gameSeq.push(rand);
     //when user pushes start, start button is disabled and only enabled again after pressing stop or refreshing the page
     $("#start").attr("disabled", "disabled");
-    console.log(gameSequence);
+    console.log(gameSeq);
     highlightColours();
-});
-
-//Click panel sequence
-$(".panel").mousedown(function() {
-  $("#"+(this.id)+"-sound")[0].load(); 
-  $("#"+(this.id)+"-sound")[0].play();
-  $(this).addClass("active");
-  userSequence.push("#" + this.id);
-  console.log(userSequence);
-   if (userSequence.length == gameSequence.length) { 
-    setTimeout(function(){ 
-      sequenceCompare();
-    }, 500);
-  }
-});
-//when user releases the left mouse click
-$(".panel").mouseup(function() {
-  $(this).removeClass("active");
 });
 
 //when user wants to stop the game can click stop or can refresh page, also clears the console
@@ -38,9 +20,28 @@ $("#stop").click(function() {
   location.reload();
 });
 
+//Click panel sequence adding Active class to the pad clicked - Optimized for Desktop
+$(".panel").mousedown(function() {
+  $("#"+(this.id)+"-sound")[0].load(); 
+  $("#"+(this.id)+"-sound")[0].play();
+  $(this).addClass("active");
+  userSeq.push("#" + this.id);
+  console.log(userSeq);
+   if (userSeq.length == gameSeq.length) { 
+    setTimeout(function(){ 
+      sequenceCompare();
+    }, 500);
+  }
+});
+//when user releases the left mouse click removes active class to not highlight
+$(".panel").mouseup(function() {
+  $(this).removeClass("active");
+});
+
+
 //Game sequence highlight adding css class .active to the panel
 function highlightColours() {
-  gameSequence.forEach(function(element, index){ 
+  gameSeq.forEach(function(element, index){ 
     setTimeout(function(){
         $(element + "-sound")[0].load();
         $(element + "-sound")[0].play(); 
@@ -52,11 +53,11 @@ function highlightColours() {
 }
 //Compare User sequence with computer sequence
 function sequenceCompare() {
-  if (userSequence.every(function(value, index) { return value === gameSequence[index]})) { 
+  if (userSeq.every(function(value, index) { return value === gameSeq[index]})) { 
     console.log("Match"); 
     gameScore++; 
     $("#score").text(gameScore); 
-    userSequence = []; 
+    userSeq = []; 
     //Limiting the game to 20 rounds
         if (gameScore == 20) { 
             $("#winner")[0].play();
@@ -66,8 +67,8 @@ function sequenceCompare() {
             $("#correct")[0].play();
             setTimeout(function() { 
             var rand = colourArray[Math.floor(Math.random() * 4)];
-            gameSequence.push(rand); 
-            console.log(gameSequence);
+            gameSeq.push(rand); 
+            console.log(gameSeq);
             highlightColours();
             }, 500);
         } 
@@ -77,7 +78,7 @@ function sequenceCompare() {
             $("#error-sound")[0].load();
             $("#error-sound")[0].play();
             alert("Almost!!Give it another Go"); 
-            userSequence = [];
+            userSeq = [];
             highlightColours();
         }
 }
